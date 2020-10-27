caa27db6dd546b95efc85308d6ec359655c81415
Maybe_QHL <1013480204@qq.com>
2020-10-27 10:36:07 +0800

    init

--- example/src/components/composition-demo.vue
+++ example/src/components/composition-demo.vue
@@ -1,36 +1,38 @@
 <template>
-  <h2>composition-demo</h2>
-  <div class="block" v-anime="{ translateX: 100 }"></div>
-  <div class="block" id="test1"></div>
-  <div class="block" id="test2"></div>
+  <h2 class="title">composition-demo</h2>
   <hr />
+  <!-- <div class="block" v-anime="{ translateX: 100 }"></div> -->
+  <transition>
+    <div class="block c-t1" v-if="isShow" v-anime="inObj"></div>
+  </transition>
+  <button @click="reset">重置动画</button>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, ref } from "vue";
 import { anime } from "@maybecode/vue-next-animejs";
+
 export default defineComponent({
   name: "App",
   components: {},
   setup() {
+    const isShow = ref(true);
+    const inObj = {
+      translateY: [{ value: 20, duration: 500 }],
+      easing: "easeOutElastic(1, .8)",
+      loop: false,
+      complete() {
+        // isShow.value = !isShow.value;
+      },
+    };
+
+    const reset = () => {
+      isShow.value = !isShow.value;
+    };
     onMounted(() => {
-      anime({
-        targets: "#test1",
-        translateX: 120,
-      });
-      anime({
-        targets: "#test2",
-        translateX: 150,
-      });
+      // animeIns = anime(obj);
     });
-    return {};
+    return { isShow, inObj, reset };
   },
 });
 </script>
-<style>
-.block {
-  width: 50px;
-  height: 50px;
-  background-color: orange;
-}
-</style>
--- example/src/components/options-demo.vue
+++ example/src/components/options-demo.vue
@@ -1,9 +1,9 @@
 <template>
-  <h2>options-demo</h2>
+  <h2 class="title">options-demo</h2>
+  <hr />
   <div class="block" v-anime="{ translateX: 100 }"></div>
   <div class="block" id="test1"></div>
   <div class="block" id="test2"></div>
-  <hr />
 </template>
 
 <script lang="ts">
@@ -23,10 +23,3 @@ export default defineComponent({
   },
 });
 </script>
-<style>
-.block {
-  width: 50px;
-  height: 50px;
-  background-color: orange;
-}
-</style>
